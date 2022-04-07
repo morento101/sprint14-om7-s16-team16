c0054ab5027afed7fc0638fb078c5ca0e3548087
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-04-07 10:40:18 +0300

    create update and delete order

--- order/urls.py
+++ order/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('order_list/', views.OrderList.as_view(), name='order_list'),
-    path('order_whitelist/', views.FilteredOrderList.as_view(), name='order_whitelist')
+    path('order_whitelist/', views.FilteredOrderList.as_view(), name='order_whitelist'),
+
+    path('create_order/', views.OrderFormView.as_view(), name='create_order'),
+    path('update_order/<int:order_id>/', views.OrderFormView.as_view(), name='update_order'),
+    path('delete_order/<int:order_id>/', views.DeleteOrderView.as_view(), name='delete_order'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.db.models import F
+from django.http import HttpResponseBadRequest
 
 from .models import Order
+from .forms import OrderForm
 
 
 class OrderList(View):
@@ -18,3 +20,34 @@ class FilteredOrderList(View):
             .order_by('created_at', 'plated_end_at') \
             .all()
         return render(request, 'order/order_list.html', context={'orders': orders})
+
+
+class OrderFormView(View):
+    def get(self, request, order_id=None):
+        if order_id:
+            order = get_object_or_404(Order, id=order_id)
+            form = OrderForm(instance=order)
+        else:
+            form = OrderForm()
+
+        return render(request, 'order/order_form.html', context={'form': form})
+
+    def post(self, request, order_id=None):
+        if order_id:
+            order = get_object_or_404(Order, id=order_id)
+            form = OrderForm(request.POST, instance=order)
+        else:
+            form = OrderForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('order_list')
+
+        return HttpResponseBadRequest('Bad Request')
+
+
+class DeleteOrderView(View):
+    def get(self, request, order_id):
+        order = get_object_or_404(Order, id=order_id)
+        order.delete()
+        return redirect('order_list')
